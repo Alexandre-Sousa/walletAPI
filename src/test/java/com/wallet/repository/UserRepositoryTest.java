97d1732877dd7524fdb896674a062407a4c6dfa7
DESKTOP-89B4AMI\alexa <alexandresousa.eng@gmail.com>
2023-07-12 00:08:39 -0300

    feat: teste repositório usuários
    Criada classe de teste para o repositório dos usuários.
    
    WALL-002

---
+++ src/test/java/com/wallet/repository/UserRepositoryTest.java
@@ -0,0 +1,31 @@
+package com.wallet.repository;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@ActiveProfiles("test")
+@SpringBootTest
+public class UserRepositoryTest {
+	
+	@Autowired
+	UserRepository repository;
+	
+	@Test
+	private void testeSave() {
+		
+		User user = new User();
+		
+		user.setName("Test");
+		user.setPassword("123456");
+		user.setEmail("test@test.com");
+		
+		User response = repository.save(user);
+		
+		assertNotNull(response);
+		
+	}
+}
